72b092489be5a2fe99c90534cc6080c2fc5d4184
Feiyang1 <plane1113@gmail.com>
2018-12-02 22:14:28 -0800

    messaging boilerplate

--- webpack/index.ts
+++ webpack/index.ts
@@ -1,10 +1,32 @@
 import * as firebase from 'firebase/app';
 import 'firebase/database';
+import 'firebase/messaging';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBNHCyZ-bpv-WA-HpXTmigJm2aq3z1kaH8",
-    authDomain: "jscore-sandbox-141b5.firebaseapp.com",
-    projectId: "jscore-sandbox-141b5"
+    apiKey: "AIzaSyBehpN9M4BSsxbryYdizPCVnGz4nzxcv_c",
+    authDomain: "havefun-1d170.firebaseapp.com",
+    databaseURL: "https://havefun-1d170.firebaseio.com",
+    projectId: "havefun-1d170",
+    storageBucket: "havefun-1d170.appspot.com",
+    messagingSenderId: "473315497267"
 });
-const db = firebase.database();
-console.log(db);
+
+
+/** MESSAGING */
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey('BKUDDOJYIsphesP-FC5o6o_fGNpzS2RMVDduHXAMlNQWXB4fWRi1HCrHqqa29MDgxzdOvc4wMCZOgpc53Zbp_QE');
+
+messaging.requestPermission()
+    .then(function () {
+        console.log('Permission granted.');
+    })
+    .catch(function (e) {
+        console.log('Error occured.');
+    });
+
+messaging.getToken().then(console.log);
+
+messaging.onMessage(function (payload) {
+    console.log('Message received.');
+    console.log(payload);
+});
